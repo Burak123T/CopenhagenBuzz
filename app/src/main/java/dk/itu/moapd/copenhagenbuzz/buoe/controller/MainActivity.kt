bef942acf2190fe64ef29637e679b27aad0285f1
syengil <syengil21@ku.edu.tr>
2025-03-30 16:27:50 +0200

    frgamentlar + agp version changed

--- app/src/main/java/dk/itu/moapd/copenhagenbuzz/buoe/controller/MainActivity.kt
+++ app/src/main/java/dk/itu/moapd/copenhagenbuzz/buoe/controller/MainActivity.kt
@@ -10,6 +10,9 @@ import com.google.android.material.snackbar.Snackbar
 import dk.itu.moapd.copenhagenbuzz.buoe.R
 import dk.itu.moapd.copenhagenbuzz.buoe.databinding.ActivityMainBinding
 import dk.itu.moapd.copenhagenbuzz.buoe.model.Event
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.fragment.app.Fragment
+
 
 /**
  * The main activity of the application.
@@ -29,18 +32,54 @@ class MainActivity : AppCompatActivity() {
     // An instance of the Event class
     private var event: Event = Event() // Initialize with empty strings
 
+    private val navListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        var selectedFragment: Fragment? = null
+
+        when (item.itemId) {
+            R.id.navigation_timeline -> selectedFragment = TimelineFragment()
+            R.id.navigation_favorites -> selectedFragment = FavoritesFragment()
+            R.id.navigation_maps -> selectedFragment = MapsFragment()
+            R.id.navigation_calendar -> selectedFragment = CalendarFragment()
+        }
+
+        // Replace the current fragment with the selected one
+        selectedFragment?.let {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.nav_host_fragment, it)
+                .commit()
+        }
+
+        true
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
 
         contentBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(contentBinding.root)
+
+
+        // Bottom navigation listener
+        contentBinding.bottomNavigation.setOnNavigationItemSelectedListener(navListener)
+
+        // Load the default fragment (Timeline)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.nav_host_fragment, TimelineFragment()) // Replace with TimelineFragment initially
+                .commit()
+        }
+
+
+
+
+        // Navigation item listener
+
+
 
         /**
          * The root view of the activity.
          */
         val view = contentBinding.root
-
-        setContentView(view)
 
         val getIsLoggedIn = intent.getBooleanExtra("isLoggedIn", false)
 
